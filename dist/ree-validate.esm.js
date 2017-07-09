5e5ef6925bbb70d12a8f10dbcd363d9b0dd59c30
Moeen Basra <m.basra@live.com>
2017-07-09 21:06:46 +0400

    1.0.14

--- dist/ree-validate.esm.js
+++ dist/ree-validate.esm.js
@@ -1,5 +1,5 @@
 /**
- * ree-validate v1.0.13
+ * ree-validate v1.0.14
  * Extended from vee-validate
  * (c) 2017 Moeen Basra
  * @license MIT
@@ -2503,7 +2503,7 @@ var index = {
   Validator,
   ErrorBag,
   Rules,
-  version: '1.0.13'
+  version: '1.0.14'
 };
 
 export default index;
