7ff2a35b4c62dffa5d391931c3ccb47ae77e12c9
Moeen Basra <m.basra@live.com>
2020-11-16 11:47:23 +0500

    WIP: convert to ts

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  trailingComma: 'none',
+  printWidth: 120,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf'
+};
